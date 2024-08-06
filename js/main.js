9855e65e0c8f129c32f26d870b361bec89a98f3e
shehap012340111 <she97468@gmail.com>
2024-08-06 12:58:33 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,165 @@
+
+
+$(".projj").owlCarousel({
+    loop:true,
+    margin:10,
+    navigation: true,
+    smartSpeed: 1500,
+    autoplay: true,
+    paginationSpeed : 5000,
+    dots: false,
+    nav: true,
+    navText:[`<i class="fa-solid fa-chevron-left"></i>`,`<i class="fa-solid fa-chevron-right"></i>`],
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+// external js: isotope.pkgd.js
+
+
+// filter functions
+var filterFns = {
+  // show if number is greater than 50
+  numberGreaterThan50: function() {
+    var number = $(this).find('.number').text();
+    return parseInt( number, 10 ) > 50;
+  },
+  // show if name ends with -ium
+  ium: function() {
+    var name = $(this).find('.name').text();
+    return name.match( /ium$/ );
+  }
+};
+
+function getHashFilter() {
+  // get filter=filterName
+  var matches = location.hash.match( /filter=([^&]+)/i );
+  var hashFilter = matches && matches[1];
+  return hashFilter && decodeURIComponent( hashFilter );
+}
+
+// init Isotope
+var $grid = $('.grid');
+
+// bind filter button click
+var $filterButtonGroup = $('.filter-button-group');
+$filterButtonGroup.on( 'click', 'button', function() {
+  var filterAttr = $( this ).attr('data-filter');
+  // set filter in hash
+  location.hash = 'filter=' + encodeURIComponent( filterAttr );
+});
+
+var isIsotopeInit = false;
+
+function onHashchange() {
+  var hashFilter = getHashFilter();
+  if ( !hashFilter && isIsotopeInit ) {
+    return;
+  }
+  isIsotopeInit = true;
+  // filter isotope
+  $grid.isotope({
+    itemSelector: '.element-item',
+    layoutMode: 'fitRows',
+    // use filterFns
+    filter: filterFns[ hashFilter ] || hashFilter
+  });
+  // set selected class on button
+  if ( hashFilter ) {
+    $filterButtonGroup.find('.is-checked').removeClass('is-checked');
+    $filterButtonGroup.find('[data-filter="' + hashFilter + '"]').addClass('is-checked');
+  }
+}
+
+$(window).on( 'hashchange', onHashchange );
+
+// trigger event handler to init Isotope
+onHashchange();
+
+
+// scroll class///////////
+
+const sections = document.querySelectorAll("section");
+const navLi = document.querySelectorAll("nav .container ul li");
+window.onscroll = () => {
+  var current = "";
+
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    if (pageYOffset >= sectionTop - 500) {
+      current = section.getAttribute("id"); }
+  });
+
+  navLi.forEach((li) => {
+    li.classList.remove("active");
+    if (li.classList.contains(current)) {
+      li.classList.add("active");
+    }
+  });
+};
+
+
+(function($) { "use strict";
+
+	//Switch dark/light
+
+	$(document).ready(function(){"use strict";
+	
+		//Scroll back to top
+		
+		var progressPath = document.querySelector('.progress-wrap path');
+		var pathLength = progressPath.getTotalLength();
+		progressPath.style.transition = progressPath.style.WebkitTransition = 'none';
+		progressPath.style.strokeDasharray = pathLength + ' ' + pathLength;
+		progressPath.style.strokeDashoffset = pathLength;
+		progressPath.getBoundingClientRect();
+		progressPath.style.transition = progressPath.style.WebkitTransition = 'stroke-dashoffset 10ms linear';		
+		var updateProgress = function () {
+			var scroll = $(window).scrollTop();
+			var height = $(document).height() - $(window).height();
+			var progress = pathLength - (scroll * pathLength / height);
+			progressPath.style.strokeDashoffset = progress;
+		}
+		updateProgress();
+		$(window).scroll(updateProgress);	
+		var offset = 50;
+		var duration = 50;
+		jQuery(window).on('scroll', function() {
+			if (jQuery(this).scrollTop() > offset) {
+				jQuery('.progress-wrap').addClass('active-progress');
+			} else {
+				jQuery('.progress-wrap').removeClass('active-progress');
+			}
+		});				
+		jQuery('.progress-wrap').on('click', function(event) {
+			event.preventDefault();
+			jQuery('html, body').animate({scrollTop: 0}, duration);
+			return false;
+		})
+		
+		
+	});
+	
+})(jQuery); 
+
+
+// /////////////////////////////////////
+let heart = document.getElementById("heart");
+
+heart.addEventListener("click", function () {
+    heart.classList.toggle("onclick-heart");
+})
+/////////////////////
+
+const checkbox = document.getElementById("checkbox")
+checkbox.addEventListener("change", () => {
+  document.body.classList.toggle("dark")
+})
